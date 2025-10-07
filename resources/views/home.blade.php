7ceac81229720be38c93b4c527e5f135af884b16
Bombiasz2721 <twardaktobiasz@gmail.com>
2025-10-07 10:24:25 +0200

    updejcik

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,25 @@
 @section('content')
 
 <div class="container" class="main-img">
-    
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    <!-- {{ $album }} -->
+    <div class="row justify-content-center flex-wrap">
+        <div class="col-md-4 ">
+
+                
+
+                <div class="albumshold ">
+                    @foreach ($albums as $album)
+                        <div class="album bg-white ">
+                            <h2>{{ $album->title }}</h2>
+                            <p>Release Date: {{ $album->release_date }}</p>
+                            <p>Genre: {{ $album->genre }}</p>
+                            <p>Label: {{ $album->label }}</p>
+                            <p>Producer: {{ $album->producer }}</p>
+                        </div>
+                        
+                    @endforeach
                 </div>
-            </div>
-        </div>
+
     </div>
 </div>
 @endsection
