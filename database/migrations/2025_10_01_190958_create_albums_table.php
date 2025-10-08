d4697f32de2be2b3ea25235c595116beb9ed7249
Fortox226 <97980099+Fortox226@users.noreply.github.com>
2025-10-08 09:40:01 +0200

    albums cover

--- database/migrations/2025_10_01_190958_create_albums_table.php
+++ database/migrations/2025_10_01_190958_create_albums_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->smallInteger('year')->nullable();
-            $table->string('cover')->nullable(); // ścieżka do okładki
+            $table->string('cover')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
         });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,21 +9,15 @@
 </section>
 
 <div class="container" class="main-img">
-
-    <div class="row justify-content-center flex-wrap">
-        <div class="col-md-4 ">
-
-                
-
-                <div class="albumshold ">
-                    @foreach ($albums->take(3) as $album)
-                        <div class="album bg-white ">
-                            <h2>{{ $album->title }}</h2>
-                        </div>
-                        
-                    @endforeach
+    <div class="row">
+        <div class="albumshold ">
+            @foreach ($albums->take(3) as $album)
+                <div class="album bg-white ">
+                    <img src="{{ $album->cover }}" alt="">
+                    <h2>{{ $album->title }}</h2>
                 </div>
-
+                        
+            @endforeach
+        </div>
     </div>
-</div>
 @endsection
