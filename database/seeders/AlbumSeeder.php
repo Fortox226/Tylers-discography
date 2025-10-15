fe58f779ac758718e5535a3ed7e05448c93a5bec
Bombiasz2721 <twardaktobiasz@gmail.com>
2025-10-15 09:39:38 +0200

    update albumy

--- database/seeders/AlbumSeeder.php
+++ database/seeders/AlbumSeeder.php
@@ -15,5 +15,66 @@ class AlbumSeeder extends Seeder
                 'album_id' => $album->id,
             ]);
         });
+        Album::create([
+            'title' => 'Bastard',
+            'year' => 2009,
+            'cover' => Null,
+            'description' => 'Debiutancki projekt (często traktowany jako mixtape) Tylera, w którym eksploruje mroczne tematy, relacje rodzinne, bunt i konflikty wewnętrzne.'
+        ]);
+        Album::create([
+            'title' => 'Goblin',
+            'year' => 2011,
+            'cover' => Null,
+            'description' => 'Drugi oficjalny album, wyróżniający się mrocznym klimatem i dialogami z fikcyjnym terapeutą Dr. TC.'
+        ]);
+        Album::create([
+            'title' => 'Wolf',
+            'year' => 2013,
+            'cover' => Null,
+            'description' => 'Projekt koncepcyjny z postaciami Wolf, Sam i Salem, kontynuujący motywy psychologiczne i konflikty wewnętrzne.'
+        ]);
+        Album::create([
+            'title' => 'Cherry Bomb',
+            'year' => 2015,
+            'cover' => Null,
+            'description' => 'Bardziej agresywny i eksperymentalny niż poprzednie, łączy elementy hip-hopu, jazzu, rocka i dźwiękowych eksperymentów.'
+        ]);
+        Album::create([
+            'title' => 'Flower Boy',
+            'year' => 2017,
+            'cover' => 'flowerboy.jpg0',
+            'description' => 'Przełomowy album pod względem emocjonalnego podejścia — tematy samotności, tożsamości, miłości; dźwięki bardziej melodyjne i miękkie.'
+        
+        ]);
+        Album::create([
+            'title' => 'Igor',
+            'year' => 2019,
+            'cover' => 'igor.jpg',
+            'description' => 'Bardzo koncepcyjny album, łączący hip-hop, soul, funk i R&B; opowieść o miłosnym trójkącie, intensywne emocje i zmiany nastrojów.'
+        ]);
+
+        Album::create([
+            'title' => 'Call Me If You Get Lost',
+            'year' => 2021,
+            'cover' => Null,
+            'description' => 'Album podróżniczy, narrowany przez DJ Drama, z motywami luksusu, introspekcji, ryzyka i tożsamości.'
+        ]);
+
+        Album::create([
+            'title' => 'Chromakopia',
+            'year' => 2024,
+            'cover' => 'chromakopia.jpg',
+            'description' => 'Wydany po przerwie, łączy hip-hop, jazz i soul; konceptualnie opowiada historię poprzez perspektywę matki Tylera, refleksje nad przeszłością.'
+        ]);
+
+        Album::create([
+            'title' => "Don't Tap the Glass",
+            'year' => 2025,
+            'cover' => Null,
+            'description' => 'Najnowszy album — bardziej energetyczny, z elementami house, funku i tańca; lżejszy ton, eksperyment z brzmieniem'
+        ]);
+
     }
 }
+
+
