85c967d0eff4880cf82d5b3c01e40736dce2b5d2
Fortox226 <97980099+Fortox226@users.noreply.github.com>
2025-10-21 09:34:29 +0200

    finish discography

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,3 +1,7 @@
-<div>
+@extends('layouts.admin')
+
+@section('content')
+<div style="background-color: #3C3D37; border-radius: 12,5px;">
     <h1>Witaj {{ Auth::user()->name }}</h1>
 </div>
+@endsection
--- resources/views/discography.blade.php
+++ resources/views/discography.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($albums as $album)
-        <div class="album-row">
-            <h2>{{ $album->title }}</h2>
-            <p>Release Date: {{ $album->year }}</p> 
-            <img src="{{ $album->cover }}" alt=""> 
-              <ul class="song-list">
-                @foreach ($album->songs as $song)
-                    <li>
-                        <strong>{{ $loop->iteration }}.</strong> {{ $song->title }}
-                        @if ($song->featuring)
-                            <em>(feat. {{ $song->featuring }})</em>
-                        @endif
-                        @if ($song->duration)
-                            – <span>{{ $song->duration }}</span>
-                        @endif
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-    @endforeach
+        @foreach ($albums as $album)
+            <div class="album">
+                <h2>{{ $album->title }}</h2>
+                <p>Release Date: {{ $album->year }}</p>
+            
+                <div class="album-content">
+                    <img src="{{ $album->cover }}" alt="{{ $album->title }} cover" class="album-cover">
+            
+                    <div class="song-list">
+                        @foreach ($album->songs as $song)
+                            <div class="song-item">
+                                <span class="song-number">{{ $loop->iteration }}.</span>
+                                <span class="song-title">{{ $song->title }}</span>
+                                @if ($song->duration)
+                                    <span class="song-duration">– {{ $song->duration }}</span>
+                                @endif
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+        @endforeach
 @endsection
